1d88750c53b0ca7b7f150e384776f6f968301797
wangcong <wangcong.merrier@bytedance.com>
2017-08-10 10:10:54 +0800

    ADD:v2.0

--- README.md
+++ README.md
@@ -2,5 +2,15 @@
 
 一个自动为你的网页添加fork按钮的原生javascript插件，简单实用！
 
-v1.0.2
+v1.0
+生成ribbon之后可重新配置参数
+
+
+
+
+v2.0
+
+样式大变样，可定制化选项更多
+
+如果hostname中有'.github.io'，将不需配置即可生成ribbon
 
--- github-fork-ribbon.js
+++ github-fork-ribbon.js
@@ -26,6 +26,18 @@
         return o;
     }
 
+    // 生成跳转链接
+    function getUrl(opt_url){
+        var hostName = location.hostname;
+        var index = hostName.indexOf('.github.io');
+
+        if(opt_url === defaults.url){   // 用户没有自定义url参数
+            opt_url = index === -1 ? opt_url : '//github.com/' + hostName.slice(0, index);
+        }
+
+        return opt_url;
+    }
+
     // 生成类名
     function classNameCreate(pos,type){
         var base = 'github-fork-ribbon';
@@ -44,45 +56,40 @@
     // 默认参数
     var defaults = {
         text : 'Fork me on Github', // 展示文本,请不要过长或过短，目前无法根据文本长度动态改变ribbon大小
-        url : 'https://github.com/merrier/', // 跳转链接
+        url : '//github.com/merrier/', // 跳转链接
         position: ['right','top'], // 位置，array类型
         type: 'black',  // 样式类型，目前只支持四种：'black','orange','red',green
         zIndex: 2,  // 层级
         fixed: false,   // 是否position设置为fixed
-        target: '_blank' // 何处打开链接，和a标签的target属性一致
+        target: '_blank', // 何处打开链接，和a标签的target属性一致
+        fontSize: '15px' // 字体大小
     };
 
     // 插件构造函数
-    var forkGithub = function(opt) {
-        forkGithub.prototype.options = extend(defaults,opt,true); //初始化时配置参数
+    var githubFork = function(opt) {
+        githubFork.prototype.options = extend(defaults,opt,true); //初始化时配置参数
 
-        return forkGithub.prototype.init(this.options);
+        return githubFork.prototype.init(this.options);
     };
 
-    forkGithub.prototype = {
-        constructor: forkGithub,
+    githubFork.prototype = {
+        constructor: githubFork,
 
         getDom: function(){
-            return document.getElementsByClassName('forkGithub-ribbon')[0];
+            return document.getElementsByClassName('github-fork-ribbon')[0];
         },
         create: function(opt,callback){
-            forkGithub.prototype.options = extend(forkGithub.prototype.options,opt,true); // 如果用户调用create()时传入参数，需要重置参数
+            githubFork.prototype.options = extend(githubFork.prototype.options,opt,true); // 如果用户调用create()时传入参数，需要重置参数
             if(!this.hasDom){
-
                 var node = document.createElement('a');
+                var url = getUrl(this.options.url);
                 node.className = classNameCreate(this.options.position,this.options.type);
-                node.setAttribute('target','');
-                node.setAttribute('href','');
-                node.setAttribute('data-ribbon','');
-                node.setAttribute('title','');
+                node.setAttribute('target',this.options.target);
+                node.setAttribute('href', url);
+                node.setAttribute('data-ribbon', this.options.text);
+                node.setAttribute('title', this.options.text);
                 node.style.zIndex = this.options.zIndex;
-
-                // var node = document.createElement('div');
-                // node.innerHTML = '<a href=' + this.options.url + '>' + this.options.text + '</a>';
-                // node.innerHTML = '<a target="' + this.options.target + '" href=' + this.options.url + '>' + 'fork me on github' + '</a>';
-                // node.className = 'forkGithub-ribbon forkGithub-ribbon-' + this.options.position;
-                // node.style.backgroundColor = this.options.bgColor;
-                // this.options.fixed ? node.style.position = 'fixed' : 'absolute';
+                node.style.fontSize = this.options.fontSize;
                 document.getElementsByTagName('body')[0].appendChild(node);
                 this.hasDom = true;
             }
@@ -124,21 +131,21 @@
     };
 
     // 初始化方法
-    forkGithub.prototype.init = function(options){
-        this.hasDom = false; //检查dom树中forkGithub的节点是否存在
+    githubFork.prototype.init = function(options){
+        this.hasDom = false; //检查dom树中githubFork的节点是否存在
         return this.create(options);
     };
 
     // 初始化实例时即调用init方法
-    forkGithub.prototype.init.prototype = forkGithub.prototype;
+    githubFork.prototype.init.prototype = githubFork.prototype;
 
     // 暴露接口(支持CMD、AMD规范)
     if (typeof module !== "undefined" && module.exports) {
-        module.exports = forkGithub;
+        module.exports = githubFork;
     } else if (typeof define === "function" && define.amd) {
-        define(function(){return forkGithub;});
+        define(function(){return githubFork;});
     } else {
-        window.forkGithub = forkGithub;
+        window.githubFork = githubFork;
     }
 
 })(window);
